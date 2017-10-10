e34caed6b2c1fd0d76bf6050c1f3d04ed2545549
Zuoxiaoxian <1980179070@qq.com>
2017-10-10 19:12:17 +0800

    查看网页所用的技术

--- 1.py
+++ 1.py
@@ -3,4 +3,13 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
-print "左孝贤"
+
+import builtwith
+def get_method_from_html(url):
+    ji_shu = builtwith.parse(url)
+    print url + "所用到的网络技术是:", ji_shu
+
+
+if __name__ == '__main__':
+    url = raw_input("输入网址:  ")
+    get_method_from_html(url)
